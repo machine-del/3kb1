759c8f2ea5fdafe07f7ecf65c50b36610b5eadfe
machine <137556120+machine-del@users.noreply.github.com>
2025-11-24 21:08:35 +0500

    feat: update files

---
+++ TestingPlatform.Application/DTOS/UserLoginDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingPlatform.Application.DTOS
+{
+    public class UserLoginDTO
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ TestingPlatform.Application/Interfaces/IAttemptRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestingPlatform.Application.DTOS;
+
+namespace TestingPlatform.Application.Interfaces
+{
+    public interface IAttemptRepository
+    {
+        Task<int> CreateAsync(AttemptDto Attempt);
+        Task UpdateAsync(AttemptDto Attempt);
+    }
+}
